14d0394d22f62b96dea54235404b476b6ca4b19c
LUIZ SANTOS <09038839@sempreuninassau.com.br>
2024-12-08 16:39:53 -0300

    exibir agendamento na home iniciado

--- Janelas/Home.Designer.cs
+++ Janelas/Home.Designer.cs
@@ -45,6 +45,12 @@
             this.ListarALL = new System.Windows.Forms.TreeView();
             this.lbTitulo = new System.Windows.Forms.Label();
             this.descriptRaiz = new System.Windows.Forms.Label();
+            this.agendaIgreja1 = new System.Windows.Forms.Label();
+            this.agendaData1 = new System.Windows.Forms.Label();
+            this.agendaDataCidadeBairro_1 = new System.Windows.Forms.Label();
+            this.agendaTema_1 = new System.Windows.Forms.Label();
+            this.agendaSermao_1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -192,6 +198,62 @@
             this.descriptRaiz.Text = "Aqui você poderá selecionar a categoria e, acessar todos os assuntos vinculados.";
             this.descriptRaiz.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // agendaIgreja1
+            // 
+            this.agendaIgreja1.AutoSize = true;
+            this.agendaIgreja1.Location = new System.Drawing.Point(12, 114);
+            this.agendaIgreja1.Name = "agendaIgreja1";
+            this.agendaIgreja1.Size = new System.Drawing.Size(57, 13);
+            this.agendaIgreja1.TabIndex = 16;
+            this.agendaIgreja1.Text = "IGREJA_1";
+            // 
+            // agendaData1
+            // 
+            this.agendaData1.AutoSize = true;
+            this.agendaData1.Location = new System.Drawing.Point(13, 131);
+            this.agendaData1.Name = "agendaData1";
+            this.agendaData1.Size = new System.Drawing.Size(48, 13);
+            this.agendaData1.TabIndex = 17;
+            this.agendaData1.Text = "DATA_1";
+            // 
+            // agendaDataCidadeBairro_1
+            // 
+            this.agendaDataCidadeBairro_1.AutoSize = true;
+            this.agendaDataCidadeBairro_1.Location = new System.Drawing.Point(12, 181);
+            this.agendaDataCidadeBairro_1.Name = "agendaDataCidadeBairro_1";
+            this.agendaDataCidadeBairro_1.Size = new System.Drawing.Size(162, 13);
+            this.agendaDataCidadeBairro_1.TabIndex = 18;
+            this.agendaDataCidadeBairro_1.Text = "ESTADO - CIDADE - BAIRRO_1";
+            // 
+            // agendaTema_1
+            // 
+            this.agendaTema_1.AutoSize = true;
+            this.agendaTema_1.Location = new System.Drawing.Point(13, 148);
+            this.agendaTema_1.Name = "agendaTema_1";
+            this.agendaTema_1.Size = new System.Drawing.Size(49, 13);
+            this.agendaTema_1.TabIndex = 19;
+            this.agendaTema_1.Text = "TEMA_1";
+            // 
+            // agendaSermao_1
+            // 
+            this.agendaSermao_1.AutoSize = true;
+            this.agendaSermao_1.Location = new System.Drawing.Point(13, 165);
+            this.agendaSermao_1.Name = "agendaSermao_1";
+            this.agendaSermao_1.Size = new System.Drawing.Size(53, 13);
+            this.agendaSermao_1.TabIndex = 20;
+            this.agendaSermao_1.Text = "SERMAO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(23, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 30);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Agendamentos";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -200,6 +262,12 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1014, 622);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.agendaSermao_1);
+            this.Controls.Add(this.agendaTema_1);
+            this.Controls.Add(this.agendaDataCidadeBairro_1);
+            this.Controls.Add(this.agendaData1);
+            this.Controls.Add(this.agendaIgreja1);
             this.Controls.Add(this.descriptRaiz);
             this.Controls.Add(this.lbTitulo);
             this.Controls.Add(this.ListarALL);
@@ -233,5 +301,11 @@
         private System.Windows.Forms.ToolStripMenuItem consultarToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem excluirToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem excluirToolStripMenuItem1;
+        private System.Windows.Forms.Label agendaIgreja1;
+        private System.Windows.Forms.Label agendaData1;
+        private System.Windows.Forms.Label agendaDataCidadeBairro_1;
+        private System.Windows.Forms.Label agendaTema_1;
+        private System.Windows.Forms.Label agendaSermao_1;
+        private System.Windows.Forms.Label label1;
     }
 }
